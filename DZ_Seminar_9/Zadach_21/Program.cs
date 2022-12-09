9db4e10aeba4918c9ea2c8c1a95a1b24f9ee4a33
Nikitka56 <119408380+Nikitka56@users.noreply.github.com>
2022-12-09 20:47:54 +0700

    Add files via upload

---
+++ DZ_Seminar_9/Zadach_21/Program.cs
@@ -0,0 +1,11 @@
+﻿static void distance(float x1, float y1,
+                     float z1, float x2,
+                     float y2, float z2)
+{
+    double d = Math.Pow((Math.Pow(x2 - x1, 2) +
+                         Math.Pow(y2 - y1, 2) +
+                         Math.Pow(z2 - z1, 2) *
+                                   1.0), 0.5);
+    Console.WriteLine("Distance is \n" + d);
+    return;
+}
---
+++ DZ_Seminar_9/Zadacha_23/Program.cs
@@ -0,0 +1,6 @@
+﻿int n = ReadInt("Введите число N: ");
+
+for (int i = 0; i <= n; i++)
+{ 
+    Console.Write($"{i*i*i} ");
+}
